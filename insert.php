953f3bea219d1f581558a0f941f54198ae0ca88b
Sukrati Agrawal <sukratiagrawal192@gmail.com>
2021-03-14 15:08:15 +0530

    Connecting the database

--- insert.php
+++ insert.php
@@ -1,46 +1,50 @@
-<?php
-$username = $_POST['firstname'];
-$password = $_POST['password'];
-$email = $_POST['mail'];
-$phone = $_POST['phone'];
-if (!empty($username) || !empty($password) || !empty($email) || !empty($phone)) {
- $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "finance";
-    //create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-    if (mysqli_connect_error()) {
-     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-     $SELECT = "SELECT email From register Where email = ? Limit 1";
-     $INSERT = "INSERT Into register (username, password, email,phone) values(?, ?, ?, ?)";
-     //Prepare statement
-     $stmt = $conn->prepare($SELECT);
-     $stmt->bind_param("s", $email);
-     $stmt->execute();
-     $stmt->bind_result($email);
-     $stmt->store_result();
-     $stmt->store_result();
-     $stmt->fetch();
-     $rnum = $stmt->num_rows;
-     if ($rnum==0) {
-      $stmt->close();
-      $stmt = $conn->prepare($INSERT);
-      $stmt->bind_param("sssi", $username, $password, $email,$phone);
-      $stmt->execute();
-      echo "signed in successfully";
-echo'<a href="login.php">ClickHere</a>';
-echo " to go back to login page";
-     } 
-else {
-      echo "Someone already registerd using this email";
-     }
-     $stmt->close();
-     $conn->close();
+        <!-- This file is to Insert the data in DB -->
+<?php include 
+'connection.php';
+    // checks whether there is any empty data or not
+    if(!empty($_POST["phone"]) && !empty($_POST["firstname"]) && !empty($_POST["email"]) && !empty($_POST["password"]))
+    {    
+        //extracting data in variables
+        $fname=$_POST['firstname'];
+        $phone=$_POST['phone'];
+        $email=$_POST['email'];    
+        $password=$_POST['password'];
+        // initializing the flag (will be used later)
+        $flag=0;
+        // code to check whether user with the same email already exist or not
+        $sql = "SELECT id,email FROM register";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $email2=$row['email'];
+                if($email==$email2)
+                    { ?>
+                        <!-- alerting error using js -->
+                        <script type="text/javascript">
+                            alert('User already exists with the same email');
+                            window.location.href='index.html';
+                        </script>
+                    <?php
+                        // updating flag if user already exist 
+                        $flag=1;
+                    }
+                }
+            }
+            
+        if($flag==0){
+            $sql = "INSERT INTO register (username,password,email,phone)
+                                VALUES ('$fname','$password','$email','$phone')";
+            if ($conn->query($sql) === TRUE) {
+                //move to welcome.php
+                header("Location: welcome.php");
+                /*
+                    setting session-parameter known as name,
+                        which will be used at welcome.php to display the name of logged in user
+                */
+                $_SESSION['firstname'] = $username;
+                die();
+            }
+        }
+
     }
-} else {
- echo "All fields are required";
- die();
-}
 ?>
--- welcome.php
+++ welcome.php
@@ -1,7 +1,23 @@
-
-<?php    
-        session_start();
-         $name=$_SESSION['name'];    
-         echo'welcome'. $name.'<br>';
-         echo'<a href="signout.php">Signout</a>';
+    <!-- This page is the front-end page which will be shown to user after register/login -->
+<?php 
+    include 'connection.php';
+        /*
+            This Page is session protected as you can see the following code checks
+                whether there is any session variable known as name is set or not. 
+                    As If not set that means user have to login/register first 
+        */
+    if(!isset($_SESSION["firstname"]))
+    {
+        header("Location: index.html");
+        die();
+    }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+    </body>
+</html>
